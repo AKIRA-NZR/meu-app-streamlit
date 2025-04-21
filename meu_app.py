e03728fff74b04a26c57db1e175812a2bfe8a26f
okaberintar <160866779+okaberintar@users.noreply.github.com>
2025-04-21 16:27:05 -0300

    Update meu_app.py

--- meu_app.py
+++ meu_app.py
@@ -3,6 +3,6 @@ import streamlit as st
 a = st.text_input("Você me ama?")
 if a:
     if a.lower() == "sim":
-        st.write("Eu também te amo chata :)")
+        st.write("Eu também te amo, chata :)")
     else:
-        st.write("Diga que sim vagabunda")
+        st.write("Diga que sim,vagabunda")
